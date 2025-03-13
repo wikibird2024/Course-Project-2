e74cd6f3dc5dd579254ad06420bee785925c1eb8
HX <tranhaobku2017@gmail.com>
2025-03-13 15:45:13 +0700

    test again

--- course_project_2/course_project_2.ino
+++ course_project_2/course_project_2.ino
@@ -1,101 +1,111 @@
+#define TINY_GSM_MODEM_SIM800  
+#include <TinyGsmClient.h>
 #include <Wire.h>
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_Sensor.h>
 
 Adafruit_MPU6050 mpu;
-HardwareSerial gpsSerial(2); // Serial2 cho module GPS (TX 16, RX 17)
+HardwareSerial gpsSerial(2);  // Serial2 cho module 4G-GPS (TX=16, RX=17)
 
-// Các biến phát hiện té ngã
-const float threshold = 18.0; // Ngưỡng gia tốc để phát hiện té ngã
+// Biến phát hiện té ngã
+const float threshold = 18.0;  // Ngưỡng gia tốc để phát hiện té ngã
 unsigned long fallStartTime = 0;
 bool fallDetected = false;
+bool smsSent = false;  // Kiểm soát gửi SMS
 
 // Số điện thoại nhận tin nhắn
-String phoneNumber = "+84xxxxxxxxx"; // Thay sdt nhan tin nhan
+const String phoneNumber = "+84xxxxxxxxx";  // Thay số điện thoại nhận SMS
 
 void setup() {
-  Serial.begin(115200);
-  gpsSerial.begin(9600, SERIAL_8N1, 16, 17); // UART giao tiếp với module 4G-GPS
+    Serial.begin(115200);
+    gpsSerial.begin(9600, SERIAL_8N1, 16, 17);  // UART giao tiếp với module 4G-GPS
 
-  // Khởi tạo MPU6050
-  if (!mpu.begin()) {
-    Serial.println("MPU6050 không tìm thấy!");
-    while (1);
-  }
-  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
-  mpu.setGyroRange(MPU6050_RANGE_250_DEG); // set gia tri cua MPU 
-  mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+    // Khởi tạo MPU6050
+    if (!mpu.begin()) {
+        Serial.println("MPU6050 không tìm thấy!");
+        while (1);
+    }
+    mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+    mpu.setGyroRange(MPU6050_RANGE_250_DEG);
+    mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
 
-  // Bật GPS trên module
-  initGPS();
+    initGPS();
 }
 
 void loop() {
-  sensors_event_t a, g, temp;
-  mpu.getEvent(&a, &g, &temp);
+    detectFall();
+}
 
-  // Tính tổng gia tốc
-  float totalAccel = sqrt(pow(a.acceleration.x, 2) + pow(a.acceleration.y, 2) + pow(a.acceleration.z, 2));
+void detectFall() {
+    sensors_event_t a, g, temp;
+    mpu.getEvent(&a, &g, &temp);
 
-  // Phát hiện té ngã
-  if (totalAccel > threshold) {
-    if (!fallDetected) {
-      fallDetected = true;
-      fallStartTime = millis();
+    float totalAccel = sqrt(a.acceleration.x * a.acceleration.x +
+                            a.acceleration.y * a.acceleration.y +
+                            a.acceleration.z * a.acceleration.z);
+
+    if (totalAccel > threshold) {
+        if (!fallDetected) {
+            fallDetected = true;
+            fallStartTime = millis();
+            smsSent = false;  // Reset trạng thái gửi SMS
+        }
+    } else {
+        if (fallDetected && (millis() - fallStartTime > 2000)) {
+            fallDetected = false;
+        }
     }
-  } else {
-    if (fallDetected && (millis() - fallStartTime > 2000)) { // Kiểm tra trạng thái té ngã trong 2 giây
-      fallDetected = false;
-    }
-  }
 
-  // Gửi tin nhắn khi phát hiện té ngã
-  if (fallDetected) {
-    sendSMSWithLocation();
-    delay(60000); // Tránh gửi tin nhắn liên tục
-  }
+    // Gửi tin nhắn nếu phát hiện té ngã và chưa gửi SMS trước đó
+    if (fallDetected && !smsSent) {
+        sendSMSWithLocation();
+        smsSent = true;
+    }
 }
 
 void initGPS() {
-  sendATCommand("AT+QGPS=1", 1000); // Bật GPS
+    sendATCommand("AT+QGPS=1", 1000);
 }
 
 String getGPSLocation() {
-  sendATCommand("AT+QGPSLOC=2", 5000); // Lấy vị trí GPS
-  String gpsData = "";
-  long startTime = millis();
-  while ((millis() - startTime) < 5000) { // Chờ dữ liệu tối đa 5 giây
-    while (gpsSerial.available()) {
-      gpsData += (char)gpsSerial.read();
+    sendATCommand("AT+QGPSLOC=2", 5000);
+
+    String gpsData = "";
+    long startTime = millis();
+    while ((millis() - startTime) < 5000) {
+        while (gpsSerial.available()) {
+            char c = gpsSerial.read();
+            gpsData += c;
+        }
     }
-  }
-  return gpsData;
+
+    int pos = gpsData.indexOf("+QGPSLOC:");
+    if (pos != -1) {
+        return gpsData.substring(pos);
+    } else {
+        return "Không lấy được vị trí GPS!";
+    }
 }
 
 void sendSMSWithLocation() {
-  String location = getGPSLocation();
-  if (location.length() < 5) {
-    Serial.println("Không lấy được vị trí GPS!");
-    return;
-  }
-  
-  String message = "Phát hiện té ngã! Vị trí: " + location;
+    String location = getGPSLocation();
+    String message = "Phát hiện té ngã! Vị trí: " + location;
 
-  sendATCommand("AT+CMGF=1", 1000); // Đặt chế độ SMS text
-  sendATCommand("AT+CMGS=\"" + phoneNumber + "\"", 1000);
-  gpsSerial.println(message);
-  delay(100);
-  gpsSerial.print((char)26); // Gửi SMS với Ctrl+Z
-  Serial.println("SMS đã gửi!");
+    sendATCommand("AT+CMGF=1", 1000);
+    sendATCommand("AT+CMGS=\"" + phoneNumber + "\"", 1000);
+    
+    gpsSerial.print(message);
+    gpsSerial.write(26);  // Ctrl+Z để gửi SMS
+    Serial.println("SMS đã gửi!");
 }
 
 void sendATCommand(String command, int timeout) {
-  gpsSerial.println(command);
-  long int time = millis();
-  while ((time + timeout) > millis()) {
-    while (gpsSerial.available()) {
-      Serial.write(gpsSerial.read());
+    gpsSerial.println(command);
+    long int time = millis();
+    while ((millis() - time) < timeout) {
+        while (gpsSerial.available()) {
+            Serial.write(gpsSerial.read());
+        }
     }
-  }
-  Serial.println();
+    Serial.println();
 }
